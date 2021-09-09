75b73bd043fee185e6740208174b181d3f05bac7
uselessgoddess <dodickgod@gmail.com>
2021-09-09 20:35:35 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,119 @@
+extern crate argparse;
+
+use std::borrow::BorrowMut;
+use std::collections::HashMap;
+use std::fs;
+use std::fs::DirEntry;
+use std::io::prelude::*;
+use std::path::Path;
+use std::process::exit;
+
+use argparse::{ArgumentParser, Store, StoreOption, StoreTrue, StoreFalse};
+use chrono::{Datelike, DateTime, NaiveDate, NaiveDateTime};
+use zip::write::FileOptions;
+
+fn smart_range_name(l: &NaiveDate, r: &NaiveDate) -> String {
+    if r.year() - l.year() != 0 {
+        format!("[{:04}-{:04}]", l.year(), r.year())
+    } else if r.month() - l.month() != 0 {
+        format!("{:04}-[{:02}-{:02}]", l.year(), l.month(), r.month())
+    } else {
+        format!("{:04}-{:02}-[{:02}-{:02}]", l.year(), l.month(), l.day(), r.day())
+    }
+}
+
+fn main() {
+    // todo!() create Options struct
+    let mut path = "".to_string();
+    let mut zip_name: Option<String> = None;
+    let mut default_format = "%Y-%m-%d.log".to_string();
+    let mut safe_paring = false;
+    let mut remove_logs = true;
+    let base_zip_name = "all_logs.zip".to_string();
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("TEMP DESCRIPTION");
+        ap.refer(&mut path).required()
+            .add_argument("PATH", Store,
+                          "Path to work folder");
+        ap.refer(&mut default_format)
+            .add_option(&["--format"], Store,
+                        "Format for file parsing");
+        ap.refer(&mut safe_paring)
+            .add_option(&["--safe-parsing"], StoreTrue,
+                        "Use safe parsing");
+        ap.refer(&mut remove_logs)
+            .add_option(&["--no-remove"], StoreFalse,
+                        "No remove log-files");
+        ap.refer(&mut zip_name)
+            .add_option(&["--zip-name"], StoreOption,
+                        "Substitute name instead of generated");
+        ap.parse_args_or_exit();
+    }
+
+    let paths = fs::read_dir(path.as_str()).unwrap(); // todo!() expect
+    let mut files: Vec<(DirEntry, NaiveDate)> = Vec::new();
+
+    for path in paths {
+        let path = path.unwrap(); // unknown error
+        let file_name = path.file_name().to_str().unwrap().to_owned(); // unknown error
+        let date_time = NaiveDate::parse_from_str(file_name.as_str(), default_format.as_str());
+        if let Ok(date_time) = date_time {
+            let date_time = date_time;
+            files.push((path, date_time));
+        } else {
+            if safe_paring {
+                println!("not parsing format \"{}\" - use `--safe-parsing`", file_name);
+                exit(1);
+            }
+        }
+    }
+
+    if files.is_empty() {
+        println!("files not found");
+        exit(0);
+    }
+
+    files.sort_by(|(_, a), (_, b)| {
+        return a.cmp(&b);
+    });
+
+    let open_name = if zip_name.is_some() {
+        zip_name.unwrap()
+    } else {
+        let len = files.len();
+        match len {
+            _ if {len >= 2} => {
+                let l = &files.first().unwrap().1;
+                let r = &files.last().unwrap().1;
+                format!("{}.zip", smart_range_name(&l, &r))
+            }
+            1 => {
+                let one = &files.first().unwrap().1;
+                format!("[{}]", one.to_string())
+            }
+            _ => { base_zip_name }
+        }
+    };
+
+    let path = std::path::Path::new(open_name.as_str());
+    let file = std::fs::File::create(&path).unwrap();
+    let mut zip = zip::ZipWriter::new(file);
+    let options =  FileOptions::default()
+        .compression_method(zip::CompressionMethod::Stored);
+
+    for (entry, _) in files.iter()
+        .take(files.len() - 1)
+    {
+        zip.start_file(entry.file_name().to_str().unwrap(), options);
+
+        let path = entry.path();
+        let mut buf = String::new();
+        let mut file = std::fs::File::open(path.as_path()).unwrap(); // 100% created
+        file.read_to_string(buf.borrow_mut());
+        zip.write_all(buf.as_bytes());
+        if remove_logs {
+            fs::remove_file(path.as_path());
+        }
+    }
+}
